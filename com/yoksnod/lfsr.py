9fc74092714971a0aad91bc844cabdd7f5a8f3f3
Dmitry Donskoy <d.donskoy@corp.mail.ru>
2015-12-11 14:16:28 +0300

    fix : change implementation algorithms - add dependency  from current timestamp opt : remove correlation methods

--- com/yoksnod/lfsr.py
+++ com/yoksnod/lfsr.py
@@ -1,53 +1,18 @@
 from functools import reduce
-from fractions import Fraction
-from cmath import rect, pi
 import getopt
 import random
 import sys
+import time
 
+current_millis_time = lambda: int(round(time.time() * 1000))
 
-def flat_list(s, q):
-    if type(s[0]) is int:
-        return s
-    elif type(s[0]) is list:
-        return [reduce(lambda i, j: (i * q) + j, e) for e in s]
+def flat_list(sequence, q):
+    if type(sequence[0]) is int:
+        return sequence
+    elif type(sequence[0]) is list:
+        return [reduce(lambda i, j: (i * q) + j, item) for item in sequence]
     else:
         raise TypeError
-
-
-def xac(s, q=None, d=None, n=None):
-    return xcc(s, s, q, d, n)
-
-
-def xcc(s1, s2, q=None, d=None, n=None):
-
-    def cc(s1, s2):
-        assert type(s1[0]) == type(s2[0])
-        if type(s1[0]) is list:
-            s3 = [[(j - i) % q for i, j in zip(u, v)] for u, v in zip(s1, s2)]
-            s4 = [reduce(lambda x, y: (x * q) + y, e) for e in s3]
-            z = sum(rect(1, 2 * pi * i / q) for i in s4) / len(s1)
-        elif type(s1[0]) is int:
-            z = sum(rect(1, 2 * pi * (j - i) / q) for i, j in zip(s1, s2)) / len(s1)
-        else:
-            raise TypeError
-        zr, zi = round(z.real, n), round(z.imag, n)
-        if abs(zi % 1) < 10 ** -n:
-            if abs(zr - round(zr)) < 10 ** -n:
-                return int(zr)
-            elif Fraction(z.real).limit_denominator().denominator <= d:
-                return Fraction(z.real).limit_denominator()
-            else:
-                return zr
-        else:
-            return complex(zr, zi)
-
-    q = 2 if q is None else q
-    d = 30 if d is None else d
-    n = 3 if n is None else n
-    assert len(s1) == len(s2)
-    return [cc(s1, s2[i:] + s2[:i]) for i in range(len(s1))]
-
 
 def LFSR(P, S, M, N, K):
     """
@@ -57,16 +22,16 @@ def LFSR(P, S, M, N, K):
     P polynom
     S initial state
     """
-    def LFSR2():
-        seq, st = [S[-1]], S
+    def lfsr_internal():
+        seq, state_next = [S[-1]], S
         for j in range(K ** len(S) - 2):
-            st0 = sum([i * j for i, j in zip(st, P[1:])]) % K
-            st = [st0] + st[:-1]
-            seq += [st[-1]]
+            state0 = sum([i * j for i, j in zip(state_next, P[1:])]) % K
+            state_next = [state0] + state_next[:-1]
+            seq += [state_next[-1]]
         return seq
 
     assert len(P) > 1 and len(P) - 1 == len(S)
-    s = LFSR2()
+    s = lfsr_internal()
     L = len(s)
     assert M <= L
     return [s[i % L] if M == 1 else (s[i % L:] + s[:i % L])[:M] for i in range(N)]
@@ -97,8 +62,13 @@ if __name__ == "__main__":
         print sys.stderr, "You are required empty sequence"
         exit(4)
 
-    basis = get_rand_basis(count)
-    print(basis)
-    sec = LFSR([1, 1, 0, 1, 1], [0, 0, 0, 1], basis, count, 2)
+    sec = LFSR([1, 1, 0, 1, 1], [0, 0, 0, 1], count, count, 2)
+
+    mod = count * count
+    seed = current_millis_time() % mod
+    seed = seed + count if seed < count else seed
+    print(seed)
+
     print("sequence =", sec)
-    print("sequence =", flat_list(sec, 2))
+    array = flat_list(sec, seed)
+    print("rand password =", ''.join(str(current) for current in array))
